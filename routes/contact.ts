bcaddc842021043dd460ae133c60c376dc2a55c1
Rhoda Mabundu <103856070+rhodamabs@users.noreply.github.com>
2022-12-13 20:54:18 +0200

    Update contact.ts

--- routes/contact.ts
+++ routes/contact.ts
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contact');
 
+import protect from '../middleware/authMiddleware';
 
-router.get('/:user_id', contactsController.getContact);
-router.put('/:user_id', contactsController.updateContact);
-
+router.post('/', protect, contactsController.insertContact);
+router.get('/:user_id',protect, contactsController.getContact);
+router.put('/:user_id', protect, contactsController.updateContact);
+router.delete('/:user_id, protect, contactsController.deleteContact
 module.exports = router;
